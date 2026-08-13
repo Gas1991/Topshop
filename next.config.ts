ed56027af039148572aa62091b470466c205d200
Ghassen Gharbi <ghassengharbi191@gmail.com>
2026-08-13 11:36:12 +0200

    fix: allow cdn.shipper.network in next/image remotePatterns
    Products using the Shipper CDN fallback image (meta_data._toprix_image_url,
    used after detaching natively-hosted images to free hosting disk quota)
    were showing broken images because next/image blocks unlisted domains.

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'wc.toprix.tn' },
       { protocol: 'https', hostname: 'shop.toprix.tn' },
       { protocol: 'https', hostname: 'spacenet.tn' },
+      { protocol: 'https', hostname: 'cdn.shipper.network' },
       { protocol: 'https', hostname: '*.wp.com' },
       { protocol: 'https', hostname: '*.woocommerce.com' },
     ],
